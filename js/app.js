ff902e3564a913041c86e3af08fb989aa560f5c4
N3DJ <toubiaou@gmail.com>
2015-02-12 20:59:50 +0100

    0.9
    add the detect colision. TODO: fix the problem with the Y axes. he
    doesn't match normally

--- js/app.js
+++ js/app.js
@@ -1,50 +1,79 @@
-// load the pictures of the app
-    Resources.load([
-        'images/char-cat-girl.png',
-        'images/Rock.png'
-    ]);
+Resources.load([
+    'images/char-cat-girl.png',
+    'images/Rock.png'
+]);
 
-// Enemies our player must avoid
-var Enemy = function(y, speed) {
+var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
+    this.x = x;
     this.y = y;
     this.speed = speed;
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+
 Enemy.prototype.update = function(dt) {
-    this.x += this.speed * dt;
+    if(this.x >= 505){
+        this.x = -100;
+    }else{
+        this.x += this.speed * dt;
+    }
     ctx.clearRect(0,0,505 , 606);
 
 }
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-
-var enemy1 = new Enemy(130,100);
-var enemy2 = new Enemy(210,50);
-var enemy3 = new Enemy(50,150);
-var enemy4 = new Enemy(210,25);
-
-var allEnemies = [enemy1,enemy2,enemy3,enemy4];
-
 
 var Player = function() {
     this.sprite = 'images/char-cat-girl.png';
     this.x = 200;
     this.y = 350;
+    this.score = 0;
 }
 
+
+var Level = function(){
+    this.allEnemies;
+}
+
+Level.prototype.levelOne = function(){
+    var enemy1 = new Enemy(-100,140,100);
+    var enemy2 = new Enemy(-100,220,50);
+    var enemy3 = new Enemy(-100,60,150);
+    this.allEnemies = [enemy1,enemy2,enemy3];
+}
+Level.prototype.levelTwo = function(){
+    var enemy1 = new Enemy(-100,140,200);
+    var enemy2 = new Enemy(-100,220,100);
+    var enemy3 = new Enemy(-100,60,300);
+    this.allEnemies = [enemy1,enemy2,enemy3];
+}
+Level.prototype.levelThree = function(){
+    var enemy1 = new Enemy(-100,140,250);
+    var enemy2 = new Enemy(-100,220,200);
+    var enemy3 = new Enemy(-100,60,300);
+    this.allEnemies = [enemy1,enemy2,enemy3];
+}
+Level.prototype.levelFour = function(){
+    var enemy1 = new Enemy(-100,140,250);
+    var enemy2 = new Enemy(-100,220,200);
+    var enemy3 = new Enemy(-100,60,300);
+    var enemy4 = new Enemy(-300,60,300);
+    this.allEnemies = [enemy1,enemy2,enemy3,enemy4];
+}
+Level.prototype.levelFive = function(){
+    var enemy1 = new Enemy(-100,140,250);
+    var enemy2 = new Enemy(-100,220,200);
+    var enemy3 = new Enemy(-100,60,300);
+    var enemy4 = new Enemy(-200,60,300);
+    this.allEnemies = [enemy1,enemy2,enemy3,enemy4];
+}
 
 Player.prototype.update = function() {
    ctx.clearRect(0,0,505,606);
 }
 
-// Draw the enemy on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite),this.x, this.y );
 }
@@ -52,25 +81,58 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(keyPressed) {
     switch (keyPressed){
         case "left":
-            player.x -= 100;
+            if(player.x > 0) {
+                if(player.x == 420){
+                    player.x -= 20;
+                }else{
+                    player.x -= 50;
+                }
+            }
             break;
         case "up":
-            player.y -= 50;
+            if (player.y <= 0){
+                player.x = 200;
+                player.y = 350;
+                player.score += 1;
+                switch (player.score){
+                    case 1 :
+                        levels.levelTwo();
+                        break;
+                    case 2 :
+                        levels.levelThree();
+                        break;
+                    case 3 :
+                        levels.levelFour();
+                        break;
+                    case 4 :
+                        levels.levelFive();
+                        break;
+                }
+            }else{
+                player.y -= 50;
+            }
             break;
         case "right":
-            player.x += 100;
+            if(player.x >= 400){
+                if(player.x == 400){
+                    player.x += 20;
+                }
+            }else {
+                player.x += 50;
+            }
             break;
         case "down":
-            player.y += 50;
+            if(player.y < 450) {
+                player.y += 50;
+            }
             break;
     }
 }
 
 var player = new Player();
+var levels = new Level();
+levels.levelOne();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -78,6 +140,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
    player.handleInput(allowedKeys[e.keyCode]);
 });
