08261ba83453a22698f466670311fe34e1543d0b
N3DJ <toubiaou@gmail.com>
2015-02-14 11:33:14 +0100

    1

--- js/app.js
+++ js/app.js
@@ -3,15 +3,34 @@ Resources.load([
     'images/Rock.png'
 ]);
 
+function contains(value, arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Enemies our player must avoid
 var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
 }
 
-
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
     if(this.x >= 505){
         this.x = -100;
     }else{
@@ -21,6 +40,7 @@ Enemy.prototype.update = function(dt) {
 
 }
 
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
@@ -29,45 +49,35 @@ var Player = function() {
     this.sprite = 'images/char-cat-girl.png';
     this.x = 200;
     this.y = 350;
-    this.score = 0;
+    this.score = 1;
 }
 
 
-var Level = function(){
-    this.allEnemies;
-}
+var Level = function(nbOfEnemys, nbOfLvls){
+    var posX = -100;
+    var posY = [140,220,300];
+    var speeds = [120,80,150];
+    var randomIndex;
+    var randomIndexWithoutDuplicate = [Math.floor(Math.random() * 3)];
+    var allEnemies = [];
+    for (var i = 0 ; i < nbOfEnemys ; i++ ){
+        if (i > 2){
+            do{
+                randomIndex = Math.floor(Math.random() * 3);
+            } while (!contains(randomIndex,randomIndexWithoutDuplicate));
 
-Level.prototype.levelOne = function(){
-    var enemy1 = new Enemy(-100,140,100);
-    var enemy2 = new Enemy(-100,220,50);
-    var enemy3 = new Enemy(-100,300,150);
-    this.allEnemies = [enemy1,enemy2,enemy3];
-}
-Level.prototype.levelTwo = function(){
-    var enemy1 = new Enemy(-100,140,200);
-    var enemy2 = new Enemy(-100,220,100);
-    var enemy3 = new Enemy(-100,300,300);
-    this.allEnemies = [enemy1,enemy2,enemy3];
-}
-Level.prototype.levelThree = function(){
-    var enemy1 = new Enemy(-100,140,250);
-    var enemy2 = new Enemy(-100,220,200);
-    var enemy3 = new Enemy(-100,300,300);
-    this.allEnemies = [enemy1,enemy2,enemy3];
-}
-Level.prototype.levelFour = function(){
-    var enemy1 = new Enemy(-100,140,250);
-    var enemy2 = new Enemy(-100,220,200);
-    var enemy3 = new Enemy(-100,300,300);
-    var enemy4 = new Enemy(-300,300,300);
-    this.allEnemies = [enemy1,enemy2,enemy3,enemy4];
-}
-Level.prototype.levelFive = function(){
-    var enemy1 = new Enemy(-100,140,250);
-    var enemy2 = new Enemy(-100,220,200);
-    var enemy3 = new Enemy(-100,300,300);
-    var enemy4 = new Enemy(-200,300,300);
-    this.allEnemies = [enemy1,enemy2,enemy3,enemy4];
+            randomIndexWithoutDuplicate.push(randomIndex);
+
+            if (randomIndexWithoutDuplicate.length == 3) {
+                randomIndexWithoutDuplicate = [];
+            }
+            allEnemies.push(new Enemy((posX - 160 * randomIndex), posY[randomIndex], (speeds[randomIndex] * nbOfLvls)));
+        }else{
+            allEnemies.push(new Enemy(posX, posY[i], (speeds[i] * nbOfLvls)));
+        }
+    }
+    this.allEnemies = allEnemies;
+
 }
 
 Player.prototype.update = function() {
@@ -94,20 +104,12 @@ Player.prototype.handleInput = function(keyPressed) {
                 player.x = 200;
                 player.y = 350;
                 player.score += 1;
-                switch (player.score){
-                    case 1 :
-                        levels.levelTwo();
-                        break;
-                    case 2 :
-                        levels.levelThree();
-                        break;
-                    case 3 :
-                        levels.levelFour();
-                        break;
-                    case 4 :
-                        levels.levelFive();
-                        break;
+                if(player.score <= 3){
+                    levels = new Level(3,player.score);
+                }else{
+                    levels = new Level(player.score, player.score);
                 }
+
             }else{
                 player.y -= 50;
             }
@@ -128,11 +130,13 @@ Player.prototype.handleInput = function(keyPressed) {
             break;
     }
 }
-
+// Now instantiate your objects.
 var player = new Player();
-var levels = new Level();
-levels.levelOne();
+var levels = new Level(3,1);
 
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
